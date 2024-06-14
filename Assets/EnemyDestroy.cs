631e8ce4aff8ea85411ec66427e56297e2bb7bc6
SatoShinta <kanten3005@gmail.com>
2024-06-14 13:57:12 +0900

    no message

--- Assets/EnemyDestroy.cs
+++ Assets/EnemyDestroy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class EnemyDestroy : MonoBehaviour
 {
-    public float counter;
+    [SerializeField] public float counter;
     // Start is called before the first frame update
     void Start()
     {
