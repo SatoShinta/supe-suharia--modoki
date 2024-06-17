d6348a2c60b6b8a2eccda9deb462fcfb2f4e14d2
SatoShinta <kanten3005@gmail.com>
2024-06-17 17:23:31 +0900

    新しい敵を作りました

---
+++ Assets/EnemyBulletDestroy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //もしこのオブジェクトがz軸の座標が80を超えたら
+        if(transform.position.z >= 80)
+        {
+            //破壊する
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //もしEnemyタグかTreeタグを持ったオブジェクトに当たったら
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Tree")
+        {
+            //破壊する
+            Destroy(gameObject);
+        }
+    }
+}
