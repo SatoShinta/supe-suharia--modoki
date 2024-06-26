46a3ad3b378eb3f67425ef8fb7c2f5117973c42f
SatoShinta <kanten3005@gmail.com>
2024-06-26 12:14:49 +0900

    UI関係作りました

--- Assets/EnemyDestroy2.cs
+++ Assets/EnemyDestroy2.cs
@@ -1,13 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class EnemyDestroy2 : MonoBehaviour
 
-{ 
- [SerializeField] public float counter;
+{
+    [SerializeField] public float counter;
     private float timer;
     public GameObject effectPrefab;
     public AudioClip sound;
@@ -15,44 +11,44 @@ public class EnemyDestroy2 : MonoBehaviour
 
     // Start is called before the first frame update
     void OnEnable()
-{
+    {
         timer = 0;
-}
+    }
 
-// Update is called once per frame
-void Update()
-{
+    // Update is called once per frame
+    void Update()
+    {
         //一秒ごとにtimerに数値を入れていく
         timer = Time.deltaTime;
 
         //もしこのオブジェクトの位置（z軸）が－10よりも手前に来るか、timerが8より大きくなったとき
-    if (transform.position.z <= -10 || timer >= 8)
-    {
-        //破壊する
-        Destroy(gameObject);
-    }
-    //counterが5より大きくなったとき
-    if (counter >= 4)
-    {
-        //破壊する
-        Destroy(gameObject);
+        if (transform.position.z <= -10 || timer >= 8)
+        {
+            //破壊する
+            Destroy(gameObject);
+        }
+        //counterが5より大きくなったとき
+        if (counter >= 4)
+        {
+            //破壊する
+            Destroy(gameObject);
             GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
             Destroy(effect, 0.5f);
             AudioSource.PlayClipAtPoint(sound, transform.position);
         }
-}
-
-
-private void OnCollisionEnter(Collision collision)
-{
-        //Bulletタグを持ったオブジェクトに当たった時
-    if (collision.gameObject.tag == "Bullet")
-    {
-            //counterを1増やす
-        Debug.LogWarning("球が当たりました");
-        counter++;
     }
 
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Bulletタグを持ったオブジェクトに当たった時
+        if (collision.gameObject.tag == "Bullet")
+        {
+            //counterを1増やす
+            Debug.LogWarning("球が当たりました");
+            counter++;
         }
+
+
+    }
 }
--- Assets/Scenes/StartButton.cs
+++ Assets/Scenes/StartButton.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
-  public void OnClickStart()
+    [SerializeField] GameObject panel;
+    private void Start()
+    {
+        
+    }
+    public void OnClickStart()
     {
         SceneManager.LoadScene("MainScene");
         Debug.Log("ìÆçÏÇµÇΩ");
     } 
+
+    public void SousaSetumeiOn()
+    {
+        panel.SetActive(true);
+        Debug.Log("dousasita ");
+    }
+
+    public void SousaSetumeiOff()
+    {
+        panel.SetActive(false);
+        Debug.Log("dousasita ");
+
+    }
 }
